d9163bc968feafb40d130f051b6ff0d1594d0bf7
Sundar Rajendran <sundar.moorthy@gmail.com>
2020-02-21 12:06:47 -0600

    Added Context Model

---
+++ src/main/java/org/redhat/fabric/commands/model/Context.java
@@ -0,0 +1,87 @@
+package org.redhat.fabric.commands.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Context {
+	
+	@SerializedName("CamelId")
+	private String contextId;
+	@SerializedName("TotalRoutes")
+	private String totalRoutes;
+	@SerializedName("StartedRoutes")
+	private String startedRoutes;
+	@SerializedName("State")
+	private String contextState;
+	public String getContextId() {
+		return contextId;
+	}
+	public void setContextId(String contextId) {
+		this.contextId = contextId;
+	}
+	public String getTotalRoutes() {
+		return totalRoutes;
+	}
+	public void setTotalRoutes(String totalRoutes) {
+		this.totalRoutes = totalRoutes;
+	}
+	public String getStartedRoutes() {
+		return startedRoutes;
+	}
+	public void setStartedRoutes(String startedRoutes) {
+		this.startedRoutes = startedRoutes;
+	}
+	public String getContextState() {
+		return contextState;
+	}
+	public void setContextState(String contextState) {
+		this.contextState = contextState;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contextId == null) ? 0 : contextId.hashCode());
+		result = prime * result + ((contextState == null) ? 0 : contextState.hashCode());
+		result = prime * result + ((startedRoutes == null) ? 0 : startedRoutes.hashCode());
+		result = prime * result + ((totalRoutes == null) ? 0 : totalRoutes.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Context other = (Context) obj;
+		if (contextId == null) {
+			if (other.contextId != null)
+				return false;
+		} else if (!contextId.equals(other.contextId))
+			return false;
+		if (contextState == null) {
+			if (other.contextState != null)
+				return false;
+		} else if (!contextState.equals(other.contextState))
+			return false;
+		if (startedRoutes == null) {
+			if (other.startedRoutes != null)
+				return false;
+		} else if (!startedRoutes.equals(other.startedRoutes))
+			return false;
+		if (totalRoutes == null) {
+			if (other.totalRoutes != null)
+				return false;
+		} else if (!totalRoutes.equals(other.totalRoutes))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Context [contextId=" + contextId + ", totalRoutes=" + totalRoutes + ", startedRoutes=" + startedRoutes
+				+ ", contextState=" + contextState + "]";
+	}
+	
+	
+}
